8c0aeed5776b9d40ecdcbd150cfe68a548426615
Eduardo Goncalvez <eduardogoncalvez00@gmail.com>
2025-03-30 19:11:05 -0400

    Implemented testing for generator

--- backend/clothing.py
+++ backend/clothing.py
@@ -16,34 +16,37 @@ class Clothing:
         self.occasion = occasion
         self.id = id
         
-        # Convert Clothing object to dictionary 
-        def to_dict(self):
-            return {
-                "image_data": self.image_data,
-                "main_category": self.main_category,
-                "sub_category": self.sub_category,
-                "style": self.style,
-                "silhouette": self.silhouette,
-                "color": self.color,
-                "pattern": self.pattern,
-                "season": self.season,
-                "occasion": self.occasion,
-                "id": self.id
-            }
+    # Convert Clothing object to dictionary 
+    def to_dict(self):
+        return {
+            "image_data": self.image_data,
+            "main_category": self.main_category,
+            "sub_category": self.sub_category,
+            "style": self.style,                
+            "silhouette": self.silhouette,
+            "color": self.color,
+            "pattern": self.pattern,
+            "season": self.season,                
+            "occasion": self.occasion,
+            "id": self.id
+        }
         
-        # Create a clothing object from a dictionary 
-        def from_dict(cls, data, image_data=None):
-            return cls(
-                image_data=image_data,
-                main_category=data.get("main_category"),
-                sub_category=data.get("sub_category"),
-                style=data.get("style"),
-                silhouette=data.get("silhouette"),
-                color=data.get("color"),
-                pattern=data.get("pattern"),
-                season=data.get("season"),
-                occasion=data.get("occasion"),
-                id=data.get("id")
-            )
+    # Create a clothing object from a dictionary 
+    def from_dict(cls, data, image_data=None):
+        return cls(
+            image_data=image_data,
+            main_category=data.get("main_category"),
+            sub_category=data.get("sub_category"),
+            style=data.get("style"),
+            silhouette=data.get("silhouette"),
+            color=data.get("color"),
+            pattern=data.get("pattern"),
+            season=data.get("season"),
+            occasion=data.get("occasion"),
+            id=data.get("id")
+        )
+    
+    def __str__(self):
+        return f"{self.main_category} {self.sub_category} {self.color} {self.season} {self.occasion}"
 
 
