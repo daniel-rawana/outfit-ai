80b21da7108209c28f1a63fee3b9e7d9e93c8184
The Kapitan. <159403179+chrisb9085@users.noreply.github.com>
2025-04-10 10:33:14 -0400

    Added headshot images to commit

--- frontend/src/contact-us.js
+++ frontend/src/contact-us.js
@@ -7,8 +7,7 @@ const teamMembers = [
     id: 1,
     name: "Daniel Rawana",
     role: "Team Leader",
-    blurb:
-      "lorem ispum doloor sit amet. lorem ispum doloor sit amet. lorem ispum doloor sit amet.",
+    blurb:"lorem ispum doloor sit amet. lorem ispum doloor sit amet. lorem ispum doloor sit amet.",
     img: "daniel.jpg",
     email: "",
   },
@@ -25,14 +24,12 @@ const teamMembers = [
     name: "Eduardo Goncalvez",
     role: "AI/ML, Backend",
     blurb: "lorem ispum doloor sit amet. lorem ispum doloor sit amet.",
-    img: placeholderImg,
   },
   {
     id: 4,
     name: "Camila Gloria",
     role: "Frontend",
     blurb: "lorem ispum doloor sit amet. lorem ispum doloor sit amet.",
-    img: placeholderImg,
   },
   {
     id: 5,
@@ -46,76 +43,66 @@ const teamMembers = [
     name: "Kevon Williams",
     role: "Backend",
     blurb: "lorem ispum doloor sit amet.",
-    img: placeholderImg,
   },
   {
     id: 7,
     name: "Miles Weatherspoon",
     role: "Project Support",
     blurb: "lorem ispum doloor sit amet.",
-    img: placeholderImg,
   },
   {
     id: 8,
     name: "Alec Borque",
     role: "AI/ML, Backend",
     blurb: "lorem ispum doloor sit amet.",
-    img: placeholderImg,
   },
   {
     id: 9,
     name: "Alessandra Uribe",
     role: "Fashion SME, Backend, Database",
     blurb: "lorem ispum doloor sit amet.",
-    img: placeholderImg,
   },
   {
     id: 10,
     name: "Laisha Brez",
     role: "Frontend",
     blurb: "lorem ispum doloor sit amet.",
-    img: placeholderImg,
   },
   {
     id: 11,
     name: "Nathan Chen",
     role: "AI/ML, Backend",
     blurb: "lorem ispum doloor sit amet.",
-    img: placeholderImg,
   },
   {
     id: 12,
     name: "Abhiram Bhogi",
     role: "AI/ML, Backend",
     blurb: "lorem ispum doloor sit amet.",
-    img: placeholderImg,
   },
   {
     id: 13,
     name: "Tyler Coy",
     role: "AI/ML",
     blurb: "lorem ispum doloor sit amet.",
-    img: placeholderImg,
   },
   {
     id: 14,
     name: "Ruben Perez",
     role: "AI/ML",
     blurb: "lorem ispum doloor sit amet.",
-    img: placeholderImg,
   },
   {
     id: 15,
     name: "Jayden Krishna",
     role: "Frontend",
     blurb: "lorem ispum doloor sit amet.",
-    img: placeholderImg,
   },
 ];
 
 const getHeadshot = (imgName) => {
   try{
-    return require(`./assets/${imgName}.jpg`);
+    return require(`./assets/headshots/${imgName}.jpg`);
   }catch(err){
     return placeholderImg;
   }
