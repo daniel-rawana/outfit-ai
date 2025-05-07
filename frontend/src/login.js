839c1e717b0fd1aa0231eeadc346825c1662cfa7
Alessandrau005 <157076152+Alessandra005@users.noreply.github.com>
2025-05-07 08:02:13 -0400

    Renamed login-page.js and added login.css for styling

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import './styling/login.css';
+
 
 function Login() {
     const [email, setEmail] = useState("");
