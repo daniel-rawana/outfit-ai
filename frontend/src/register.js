df61cbca0e08e6c717f351ec3af9a3026e9b6ce1
Laibrez <laishabj@gmail.com>
2025-05-27 00:58:27 -0400

    Frontend

--- frontend/src/register.js
+++ frontend/src/register.js
@@ -10,7 +10,7 @@ function Register() {
     const [error, setError] = useState("");
     const navigate = useNavigate();
     const handleRegister = async (e) => {
-    
+
         e.preventDefault();
         try {
             const res = await axios.post("http://127.0.0.1:5000/api/auth/register", {
