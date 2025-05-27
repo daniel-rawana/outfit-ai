2a2d6646ba99110c4394449fc4790991f4d8e888
Laibrez <laishabj@gmail.com>
2025-05-26 22:11:08 -0400

    frontend updates

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import './styling/login.css';
 
 function Login() {
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
--- frontend/src/register.js
+++ frontend/src/register.js
@@ -61,4 +61,5 @@ function Register() {
     );
 }
 
+
 export default Register;
